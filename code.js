a9820b66f09a4e5cc4939aa3813672e8b5a8fb85
22dcolli <144738226+22dcolli@users.noreply.github.com>
2024-09-03 10:03:04 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,4 @@
+/*
 function revInsertionSort(arr) {
   for(var i = arr.length-1; i >=0; i--) {
     var val = arr[i];
@@ -9,3 +10,23 @@ function revInsertionSort(arr) {
   }
   return arr;
 }
+*/
+function insertionSortReverse(arr) 
+{
+  //originally for(var i = arr.length-1; i > 1; i--)
+  for(var i = arr.length-1; i >= 0; i--) 
+  {
+    var val = arr[i];
+    var j;
+    //originally had this the same, but she pointed out I should reverse this
+    for(j = i; j < arr.length-1  && arr[j+1] < val; j++) 
+    {
+      //missed reversing this too
+      arr[j] = arr[j+1];
+    }
+    arr[j] = val;
+    //console.log(arr)
+  }
+  return arr;
+}
+//console.log(insertionSort(arr))
